bfa91bf7acb5b5897b50f65ea4a631b263ecee15
Kemberly-Amaya <as0021032020@unab.edu.sv>
2021-09-09 12:20:01 -0600

    parcial

---
+++ KemberlyCatalinaAmayaSerrano/Controllers/CajeroController.cs
@@ -0,0 +1,34 @@
+﻿using KemberlyCatalinaAmayaSerrano.Models.VIEWSMODEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KemberlyCatalinaAmayaSerrano.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Cajero
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult Monto(CajeroViewsModels cajero)
+        {
+            int ConvertMonto = Convert.ToInt32(cajero.Monto);
+            if (ConvertMonto % 5 == 0)
+            { 
+            
+            
+            
+            
+            }
+
+                return View();
+        }
+
+       
+    }
+}
---
+++ KemberlyCatalinaAmayaSerrano/Models/VIEWSMODEL/CajeroViewsModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KemberlyCatalinaAmayaSerrano.Models.VIEWSMODEL
+{
+    public class CajeroViewsModels
+    {
+        [Display(Name = "Monto")]
+        [Required(ErrorMessage = "Este campo es requerido")]   
+        public String Monto { get; set; }
+    }
+}
